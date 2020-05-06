4b6357428a4c979b68732fad7c81cdf52cf2c8e3
strengthofthemountain <strengthofthemountain@gmail.com>
2020-05-06 16:59:19 -0400

    mvp done

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import Followers from './Followers';
+import { CardImg, CardHeader, Card, CardBody, CardText } from 'reactstrap';
+import styled from "styled-components";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+
+const SpaceBorder = styled.div`
+  color: grey;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  display:flex;
+  flex-direction:column;
+  width: 75%;
+  background: black;
+  padding-bottom: 5%;
+`;
+
+
+class App extends React.Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/thetechsurgeon')
+    .then(response => {
+    this.setState({
+      user: response.data
+    });
+    })
+    .catch(error => {
+      console.log("error", error)
+    });
+    axios.get('https://api.github.com/users/thetechsurgeon/followers')
+    .then(response => {
+    this.setState({
+      followers: response.data
+    });
+    })
+    .catch(error => {
+      console.log("error", error)
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SpaceBorder>
+        <h1>Ben Github</h1>
+        <Card>
+        <CardBody>
+        <CardHeader>Github User: {this.state.user.name}</CardHeader>
+        <CardText>Location: {this.state.user.location}</CardText>
+        <CardText>Following: {this.state.user.following}</CardText>
+        <CardText>Followers: {this.state.user.followers}</CardText>
+        <CardImg className="cards" src={this.state.user.avatar_url}></CardImg>
+        </CardBody>
+        </Card>
+        </SpaceBorder>
+        <Followers followers={this.state.followers}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-github-user-card/src/Followers.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+import { Card, CardBody, CardTitle, CardText, CardImg, CardSubtitle, CardHeader } from 'reactstrap';
+import styled from "styled-components";
+
+const SpaceBorder = styled.div`
+  color: grey;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  
+  width: 33%;
+  background: black;
+  
+  box-shadow: 5px 10px;
+`;
+
+function Followers(props) {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+
+    return (
+      <>
+      {props.followers.map(follower => (
+                  <div key={follower.id}>
+                    <SpaceBorder>
+                    <Card>
+                    <CardBody>
+                    <CardImg className="cards" src={follower.avatar_url} />
+                    <CardHeader>UserName: {follower.login}</CardHeader>
+                    <CardText>
+                      Following: {follower.following_url.length}
+                    </CardText>
+                    <CardText>
+                      Followers: {follower.followers_url.length}
+                    </CardText>
+                    </CardBody>
+                    </Card>
+                    </SpaceBorder>
+
+
+                  </div>
+                ))}
+      </>
+    );
+
+}
+
+export default Followers;
+
